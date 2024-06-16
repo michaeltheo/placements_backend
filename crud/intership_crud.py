c5f726a2e8fea0bd76593cbafb2e31a786ef5e06
mtheo <michaeltheochar@gmail.com>
2024-06-16 21:40:23 +0300

    feat: update enums for department internships update create update internship endpoint

--- crud/intership_crud.py
+++ crud/intership_crud.py
@@ -6,7 +6,8 @@ from starlette import status
 
 from crud.company_crud import get_company
 from crud.user_crud import get_user_by_id
-from models import Internship as InternshipModel, InternshipProgram, InternshipStatus, Users, Companies, Dikaiologitika
+from models import Internship as InternshipModel, InternshipProgram, InternshipStatus, Users, Companies, Dikaiologitika, \
+    Department
 from schemas.internship_schema import InternshipCreate, InternshipAllRead
 
 
@@ -22,11 +23,30 @@ def create_or_update_internship(db: Session, user_id: int, internship_data: Inte
     Returns:
     - InternshipModel: The created or updated internship.
     """
+    # Update an existing internship (used for admins to add start date end date)
+    if internship_data.id:
+        # Find internship by internship_id
+        existing_internship = db.query(InternshipModel).filter_by(id=internship_data.id).first()
+        if not existing_internship:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Η πρακτική άσκηση δεν βρέθηκε")
+
+        # Update existing internship
+        existing_internship.company_id = internship_data.company_id
+        existing_internship.department = internship_data.department
+        existing_internship.program = internship_data.program
+        existing_internship.start_date = internship_data.start_date
+        existing_internship.end_date = internship_data.end_date
+        db.commit()
+        db.refresh(existing_internship)
+        return existing_internship
+
+    # Update internship by user (used from students to update their internship add company)
     existing_internship = get_user_internship(db, user_id)
 
     if existing_internship:
         # Update existing internship
         existing_internship.company_id = internship_data.company_id
+        existing_internship.department = internship_data.department
         existing_internship.program = internship_data.program
         existing_internship.start_date = internship_data.start_date
         existing_internship.end_date = internship_data.end_date
@@ -43,6 +63,7 @@ def create_or_update_internship(db: Session, user_id: int, internship_data: Inte
             user_id=user_id,
             company_id=internship_data.company_id,
             program=internship_data.program,
+            department=internship_data.department,
             start_date=internship_data.start_date,
             end_date=internship_data.end_date,
             status=InternshipStatus.PENDING_REVIEW
@@ -92,41 +113,6 @@ def get_user_internship(db: Session, user_id: int) -> Optional[InternshipModel]:
     return db.query(InternshipModel).filter(InternshipModel.user_id == user_id).first()
 
 
-def get_internships_by_company(
-        db: Session,
-        company_id: int,
-        program: Optional[InternshipProgram] = None,
-        internship_status: Optional[InternshipStatus] = None,
-        page: int = 1,
-        items_per_page: int = 10
-) -> Tuple[List[InternshipModel], int]:
-    """
-    Get internships by company, with optional filtering by program and pagination.
-
-    Parameters:
-    - db (Session): Database session.
-    - company_id (int): The ID of the company.
-    - program (Optional[InternshipProgram]): The program to filter by.
-    - page (int): The page number for pagination.
-    - items_per_page (int): The number of items per page.
-
-    Returns:
-    - Tuple[List[InternshipModel], int]: A list of internships and the total count.
-    """
-    query = db.query(InternshipModel).filter(InternshipModel.company_id == company_id)
-
-    if program:
-        query = query.filter(InternshipModel.program == program)
-    if internship_status:
-        query = query.filter(InternshipModel.status == internship_status)
-
-    total_items = query.count()
-    offset = (page - 1) * items_per_page
-    internships = query.offset(offset).limit(items_per_page).all() if items_per_page != -1 else query.all()
-
-    return internships, total_items
-
-
 def get_internship_by_id(db: Session, internship_id: int) -> Optional[InternshipModel]:
     """
     Get an internship by its ID.
@@ -170,18 +156,20 @@ def get_all_internships(
         db: Session,
         internship_status: Optional[InternshipStatus] = None,
         program: Optional[InternshipProgram] = None,
+        department: Optional[Department] = None,
         user_am: Optional[str] = None,
         company_name: Optional[str] = None,
         page: int = 1,
         items_per_page: int = 10
 ) -> Tuple[List[InternshipAllRead], int]:
     """
-    Get all internships with optional filtering by status, program, user academic number, and company name, with pagination.
+    Get all internships with optional filtering by status, program, department, user academic number, and company name, with pagination.
 
     Parameters:
     - db (Session): Database session.
     - internship_status (Optional[InternshipStatus]): The status to filter by.
     - program (Optional[InternshipProgram]): The program to filter by.
+    - department (Optional[Department]): The department to filter by.
     - user_am (Optional[str]): The academic number to filter by.
     - company_name (Optional[str]): The company name to filter by.
     - page (int): The page number for pagination.
@@ -192,6 +180,8 @@ def get_all_internships(
     """
     query = db.query(InternshipModel)
 
+    if department:
+        query = query.filter(InternshipModel.department == department)
     if internship_status:
         query = query.filter(InternshipModel.status == internship_status)
     if program:
@@ -209,10 +199,12 @@ def get_all_internships(
     for internship in internships:
         user = get_user_by_id(db, internship.user_id)
         company = get_company(db, internship.company_id)
+        print(company)
         internship_reads.append(
             InternshipAllRead(
                 id=internship.id,
                 user_id=internship.user_id,
+                department=internship.department,
                 program=internship.program,
                 start_date=internship.start_date,
                 end_date=internship.end_date,
@@ -220,7 +212,8 @@ def get_all_internships(
                 user_first_name=user.first_name if user else "",
                 user_last_name=user.last_name if user else "",
                 user_am=user.AM if user else "",
-                company_name=company.name if company else None
+                company_name=company.name if company else None,
+                company_id=company.id if company else None,
             )
         )
 
